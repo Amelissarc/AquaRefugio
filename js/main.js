45709af469eda9eb9695f08cdd45b63aae6f6d3b
Amelissarc <melissa.rinconft@gmail.com>
2022-12-26 21:34:36 -0300

    mensaje del commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*  console.log ("Hi world");
+document.getElementById ("myTitle").innerHTML= "We go";
+
+//------------------------------------------------------
+
+//Class 01 - Generals concepts __ syntax and variables
+console.log ("class 01");
+
+//Variables
+
+/* We don´t use "VAR" as it can be declared more then once. 
+var name = "Santiago";
+var name = "Luis";
+console.log(name) ; 
+*/
+
