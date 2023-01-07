5aa8cf71c25694a0dc61dbac221d1ee690ce38a9
Amelissarc <melissa.rinconft@gmail.com>
2023-01-06 22:02:34 -0300

    sistema de reservas con javascript ultimas modificaciones

--- js/main.js
+++ js/main.js
@@ -1,18 +1,52 @@
+/* Angie Melissa Rincón Cely 
+Esto es un simulador de reservas 
+en el sitio web solo ir a page hospedaje es donde esta vinculado con js 
+por ahora se maneje correciones sencillas que esten en string nombre apellido 
+en Number huespedes y fechas y las opciones que esten dentro del rango de elegir.
+Fecha esta pensado por ahora solo con numero de día ya que el uso del DATE no se ha visto.
+*/
+
+
 function datosReserva(){
-    let nombreIngresado = prompt("Ingresar nombre: ");
-    console.log("El nombre ingresado es " + nombreIngresado);
+    let nombreIngresado;
+    do {
+        nombreIngresado = prompt("Ingresar nombre: ");
+        if (!isNaN(nombreIngresado) === false)
+        console.log("Por favor ingrese su nombre");
+    } while(isNaN(nombreIngresado) === false)
+    console.log("El nombre ingresado es " + String(nombreIngresado));
 
-    let apellidoIngresado = prompt("Ingresar apellido: ");
-    console.log("El apellido ingresado es " + apellidoIngresado)
+    let apellidoIngresado;
+    do {
+        apellidoIngresado = prompt("Ingresar apellido: ");
+        if (!isNaN(apellidoIngresado) === false)
+        console.log("Por favor ingrese su apellido");
+    } while(isNaN(apellidoIngresado) === false)
+    console.log("El apellido ingresado es " + String(apellidoIngresado));
     
-    let cantidadHuespedes = prompt("Seleccione cantidad de huéspedes: ");
-    console.log("La reserva se realizara para " + cantidadHuespedes + " personas.")
+    let cantidadHuespedes;
+    do {
+        cantidadHuespedes = prompt("Seleccione cantidad de huéspedes: ");
+        if(isNaN(cantidadHuespedes) === true)
+        console.log("Ingrese número");
+    } while(isNaN(cantidadHuespedes)=== true)
+    console.log("La reserva se realizara para " + (Number(cantidadHuespedes)) + " personas.");
 
-    let fechaIngreso = prompt("Ingrese fecha de llegada:");
-        console.log("La fecha de llegada es: " + fechaIngreso)
+    let fechaIngreso;
+    do {
+        fechaIngreso = prompt("Ingrese fecha de check-in:");
+        if(isNaN(fechaIngreso) === true)
+        console.log("Ingrese valores numéricos");
+    } while (isNaN(fechaIngreso) === true)
+    console.log("La fecha de check-in: " + Number(fechaIngreso));
     
-    let fechaSalida = prompt("Ingrese fecha de salida:");
-    console.log("la fecha de salida es: " + fechaSalida)
+    let fechaSalida;
+    do {
+        fechaSalida = prompt("Ingrese fecha de check-out:");
+        if(isNaN(fechaSalida)=== true)
+        console.log("Ingrese valores numéricos");
+    } while (isNaN(fechaSalida) === true)
+    console.log("la fecha de check-out: " + Number(fechaSalida))
 
     let opciones = prompt("Elegí el hospedaje que deseas: \n1-Hotel. \n2-Cabaña. \n3-Camping. \nPresiona X para finalizar." );
     while(opciones !='X' && opciones !='x'){
@@ -27,16 +61,12 @@ function datosReserva(){
             case '3':
                 alert("Seleccionaste Camping");
                 break;
-
             default:
                 alert("Elegiste una opción inválida");
                 break;
         } 
-
         opciones = prompt("Elegí el hospedaje que deseas: \n1-Hotel. \n2-Cabaña. \n3-Camping. \nPresiona X para finalizar." );
     }   
-
     alert('Reserva realizada con éxito') 
 }
-
 datosReserva();
