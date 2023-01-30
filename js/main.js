82f99231654bd779cc17980f7706a90a066641ff
Amelissarc <melissa.rinconft@gmail.com>
2023-01-29 21:04:23 -0300

    agregando modificaciones js page hospedaje

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-// Angie Melissa Rincón Cely - 2da Pre-entrega
-
-
+// Angie Melissa Rincón Cely - 2da Pre-entreg
 const tomarReserva = () => {
-    let nombre = prompt("Escribe el nombre de quien realizará la reserva")
-    let cantidad = prompt ("¿Cuantos huéspedes van a ser?")
-    console.log(`La reserva se guardo con el nombre de ${nombre}; se van a alojar ${cantidad} personas.`)
-}
+    let nombre = prompt("Escribe el nombre de quien realizará la reserva");
+    let cantidad = prompt("¿Cuantos huéspedes van a ser?");
+    localStorage.setItem("nombre", nombre);
+    localStorage.setItem("cantidad", cantidad);
+    console.log(`La reserva se guardo con el nombre de ${nombre}; se van a alojar ${cantidad} personas.`);
+};
 tomarReserva();
 
 class hospedaje {
-    constructor(tipo,precio) {
+    constructor(tipo, precio) {
         this.tipo = tipo;
         this.precio = precio;
         this.info = `El hospedaje seleccionado es en ${this.tipo}, el valor por noche es de $${this.precio}USD`;
     }
-    verInfo(){
-        console.log(this.info)
+    verInfo() {
+        console.log(this.info);
     }
 }
 
@@ -24,52 +24,54 @@ let opciones = prompt("1: cabaña, 2: hotel, 3: camping");
 
 if (opciones == 1) {
     const cabaña = new hospedaje("cabaña", 250);
+    localStorage.setItem("hospedaje", "cabaña");
+    localStorage.setItem("precio", 250);
     cabaña.verInfo();
-}
-
-else if (opciones == 2) {
+} else if (opciones == 2) {
     const hotel = new hospedaje("hotel", 350);
+    localStorage.setItem("hospedaje", "hotel");
+    localStorage.setItem("precio", 350);
     hotel.verInfo();
-} 
-
-else if (opciones == 3) {
+} else if (opciones == 3) {
     const camping = new hospedaje("camping", 120);
+    localStorage.setItem("hospedaje", "camping");
+    localStorage.setItem("precio", 120);
     camping.verInfo();
+} else {
+    alert("Por favor seleccione una opción valida");
 }
-else {
-    alert("Por favor seleccione una opción valida")
-}
 
+let fechaIngreso;
+do {
+    fechaIngreso = prompt("Ingrese fecha de check-in:");
 
-function calendario() {
+    if(!fechaIngreso)
+    console.log("Ingrese fecha en formato dia/mes/año");
 
-    let fechaIngreso;
-    do {
-        fechaIngreso = prompt("Ingrese fecha de check-in:");
+    if(isNaN(fechaIngreso))
+    console.log("Ingrese fecha en formato dia/mes/año");
 
-        if(!fechaIngreso)
-        console.log("Ingrese fecha en formato dia/mes/año");
+} while (isNaN(fechaIngreso))
+console.log("La fecha de check-in: " + fechaIngreso);
 
-        if(isNaN(fechaIngreso))
-        console.log("Ingrese fecha en formato dia/mes/año");
+let fechaSalida;
+do {
+    fechaSalida = prompt("Ingrese fecha de check-out:");
 
-    } while (isNaN(fechaIngreso))
-    console.log("La fecha de check-in: " + fechaIngreso);
-    
-    let fechaSalida;
-    do {
-        fechaSalida = prompt("Ingrese fecha de check-out:");
+    if(!fechaSalida)
+    console.log("Ingrese fecha en formato dia/mes/año");
 
-        if(!fechaSalida)
-        console.log("Ingrese fecha en formato dia/mes/año");
+    if(isNaN(fechaSalida))
+    console.log("Ingrese fecha en formato dia/mes/año");
 
-        if(isNaN(fechaSalida))
-        console.log("Ingrese fecha en formato dia/mes/año");
+} while (isNaN(fechaSalida))
+console.log("la fecha de check-out: " + fechaSalida)
 
-    } while (isNaN(fechaSalida))
-    console.log("la fecha de check-out: " + fechaSalida)
-}
-calendario();
+localStorage.setItem('checkIn', fechaIngreso);
+localStorage.setItem('checkOut', fechaSalida);
+
+let checkIn = localStorage.getItem('checkIn');
+let checkOut = localStorage.getItem('checkOut');
 
 alert('Reserva realizada con éxito') 
 
@@ -79,32 +81,35 @@ let rooms = {
     habitaciones: 5,
     Camping: 2
   };
-  
-  $("#booking-form").submit(function(event) {
-    event.preventDefault(); 
-  
-    let roomType = $("#room-type").val();
-    let checkIn = new Date($("#check-in").val());
-    let checkOut = new Date($("#check-out").val());
-  
-    
-    if (checkIn >= checkOut) {
-      alert("La fecha de salida debe ser posterior a la fecha de entrada");
-      return;
-    }
 
-    if (rooms[roomType] > 0) {
-    
-      rooms[roomType]--;
-  
-    
-      $("#availability").html("Reserva exitosa, su tipo de habitación es en " + roomType + " Check-in: " + checkIn + " Check-out: " + checkOut);
-    } else {
-      
-      $("#availability").html("Lo sentimos, el tipo de habitación seleccionada no esta disponible durante las fechas seleccionadas, probá con otra.");
-    }
-  });
-  
+// Retrieve the stored state of the `rooms` object from local storage (if it exists)
+const storedRooms = JSON.parse(localStorage.getItem('rooms')) || rooms;
+
+$("#booking-form").submit(function(event) {
+  event.preventDefault(); 
+
+  let roomType = $("#room-type").val();
+  let checkIn = new Date($("#check-in").val());
+  let checkOut = new Date($("#check-out").val());
+
+  if (checkIn >= checkOut) {
+    alert("La fecha de salida debe ser posterior a la fecha de entrada");
+    return;
+  }
+
+  if (storedRooms[roomType] > 0) {
+
+    storedRooms[roomType]--;
+
+    $("#availability").html("Reserva exitosa, su tipo de habitación es en " + roomType + " Check-in: " + checkIn + " Check-out: " + checkOut);
+  } else {
+
+    $("#availability").html("Lo sentimos, el tipo de habitación seleccionada no esta disponible durante las fechas seleccionadas, probá con otra.");
+  }
+
+  // Store the updated state of the `rooms` object in local storage
+  localStorage.setItem('rooms', JSON.stringify(storedRooms));
+
   let hotelRooms = [
     {number: 1, type: 'double', occupied: false},
     {number: 2, type: 'single', occupied: true},
@@ -112,8 +117,11 @@ let rooms = {
     {number: 4, type: 'single', occupied: true},
     {number: 5, type: 'double', occupied: false}
   ];
-  
+
   let habitacionesDoblesDesocupadas = hotelRooms.filter(room => room.type === 'double' && room.occupied === false);
-  
+
   let room2 = hotelRooms.find(room => room.number === 2);
+
+});
+  
   
