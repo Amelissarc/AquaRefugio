bac21a1fc4027a1c5a22fa7bd7850ecd464001df
Amelissarc <melissa.rinconft@gmail.com>
2023-02-20 22:05:46 -0300

    modificaciones js

--- js/main.js
+++ js/main.js
@@ -1,92 +1,167 @@
 // Angie Melissa Rincón Cely - 3da Pre-entrega
 
-// Definiendo elementos de formulario y entradas
+// Definir elementos del formulario y entradas
 const form = document.querySelector('#booking-form form');
 const nameInput = document.querySelector('#specificSizeInputName');
 const guestsSelect = document.querySelector('#specificSizeSelect');
-const roomType = document.querySelector('#room-type');
-const bookingOption = document.querySelector('input[name="bookingOption"]');
-const checkIn = document.querySelector('input[name="checkIn"]');
-const checkOut = document.querySelector('input[name="checkOut"]');
+const roomTypeSelect = document.querySelector('#room-type');
+const checkInInput = document.querySelector('input[name="checkIn"]');
+const checkOutInput = document.querySelector('input[name="checkOut"]');
 
-// Función para guardar datos en el almacenamiento local
-function saveData() {
-  localStorage.setItem("name", nameInput.value);
-  localStorage.setItem("guests", guestsSelect.value);
-  localStorage.setItem("checkIn", checkIn.value);
-  localStorage.setItem("checkOut", checkOut.value);
-  localStorage.setItem("roomType", roomType.value);
-}
-
-// Función para validar datos de formulario
+// Función para validar los datos del formulario
 function validateForm() {
-  if (!checkIn.value || isNaN(Date.parse(checkIn.value))) {
-    // console.error("Seleccione una fecha de check-in válida");
+  // Comprobar que el nombre no esté vacío
+  if (nameInput.value.trim() === '') {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Por favor ingrese su nombre!',
+      footer: '<a href="">Why do I have this issue?</a>'
+    });
     return false;
   }
-  if (!checkOut.value || isNaN(Date.parse(checkOut.value))) {
-    // console.error("Por favor, seleccione una fecha de salida válida");
+
+  // Comprobar que se haya seleccionado el tipo de habitación
+  if (roomTypeSelect.value === '') {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Por favor, seleccione el tipo de habitación.',
+      footer: '<a href="">Why do I have this issue?</a>'
+    });
     return false;
   }
+
+  // Comprobar que se haya seleccionado un número válido de huéspedes
+  const numGuests = parseInt(guestsSelect.value);
+  if (isNaN(numGuests) || numGuests < 1) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Por favor, seleccione un número válido de huéspedes..',
+      footer: '<a href="">Why do I have this issue?</a>'
+    });
+    return false;
+  }
+
+  // Comprobar que se hayan ingresado fechas de check-in y check-out válidas
+  const checkInDate = new Date(checkInInput.value);
+  const checkOutDate = new Date(checkOutInput.value);
+  if (isNaN(checkInDate.getTime()) || isNaN(checkOutDate.getTime())) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Por favor, seleccione fechas de check-in y check-out válidas.',
+      footer: '<a href="">Why do I have this issue?</a>'
+    });
+    return false;
+  }
+
+  // Comprobar que la fecha de check-in sea anterior a la fecha de check-out
+  if (checkInDate >= checkOutDate) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'La fecha de check-in debe ser anterior a la fecha de check-out.',
+      footer: '<a href="">Why do I have this issue?</a>'
+    });
+    return false;
+  }
+
   return true;
 }
 
-// Detector de eventos de envío de formularios
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+// Función para guardar los datos de reserva en el almacenamiento local
+function saveBooking() {
+  localStorage.setItem('name', nameInput.value);
+  localStorage.setItem('guests', guestsSelect.value);
+  localStorage.setItem('checkIn', checkInInput.value);
+  localStorage.setItem('checkOut', checkOutInput.value);
+  localStorage.setItem('roomType', roomTypeSelect.value);
+}
 
-  // Validar formulario
-  if (!validateForm()) {
-    return false;
-  }
-
-  // Guardar datos en el almacenamiento local
-  saveData();
-
-  // Mostrar mensaje de éxito
+// Función para mostrar un mensaje de éxito
+function showSuccessMessage() {
   Swal.fire(
     'Reservado',
     'Por favor, revise su correo electrónico para confirmar sus datos',
     'success'
   );
+}
 
-  // Restablecer formulario
+// Manejar el envío del formulario
+form.addEventListener('submit', (event) => {
+  // Prevenir el comportamiento predeterminado del formulario
+  event.preventDefault();
+
+  // Validar el formulario
+  if (!validateForm()) {
+    return false;
+  }
+
+  // Guardar los datos de reserva en el almacenamiento local
+  saveBooking();
+
+  // Mostrar el mensaje de éxito
+  showSuccessMessage();
+
+  // Restablecer el formulario
   form.reset();
 });
 
-// Cargar datos desde el almacenamiento local
-const name = localStorage.getItem("name");
-const guests = localStorage.getItem("guests");
-const checkInValue = localStorage.getItem("checkIn");
-const checkOutValue = localStorage.getItem("checkOut");
-
-// Check-in 
-if (!checkIn) {
-} else if (!checkIn.value || isNaN(Date.parse(checkIn.value))) {
-  // console.log(`Seleccione una fecha de entrada válida`);
-} else {
-  const checkInDate = checkIn.value;
-  // console.log(`Fecha de ingreso: ${checkInDate}`);
-  localStorage.setItem("checkIn", checkInDate);
+// Función para crear una instancia de BookingOption a partir de un objeto de opción
+function createBookingOption(option) {
+  return new BookingOption(option.name, option.price, option.description, option.services);
 }
 
-// Check-out
-if (!checkOut) {
-} else if (!checkOut.value || isNaN(Date.parse(checkOut.value))) {
-  // console.log(`Seleccione una fecha de entrada válida`);
-} else {
-  const checkOutDate = checkOutInput.value;
-  // console.log(`Fecha de salida: ${checkOutDate}`);
-  localStorage.setItem("checkOut", checkOutDate);
-}
+// Cargar opciones de reserva desde el archivo JSON
+fetch('../js/booking_options.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Error al obtener opciones. Código de estado HTTP: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Almacenar los datos en una variable y en el localStorage
+    const bookingOptions = data.options.map(createBookingOption);
+    localStorage.setItem('bookingOptions', JSON.stringify(bookingOptions));
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
-class BookingOption {
-  constructor(option) {
-    this.name = option.name;
-    this.price = option.price;
-    this.description = option.description;
-    this.services = option.services || [];
+// Obtener las opciones de reserva del localStorage
+const bookingOptions = JSON.parse(localStorage.getItem('bookingOptions')) || [];
+
+// Mostrar opciones de reserva en la lista desplegable de opciones de reserva
+document.addEventListener('DOMContentLoaded', function () {
+  const roomTypeSelect = document.querySelector('#room-type');
+  if (roomTypeSelect) {
+    // Rellenar las opciones de la lista desplegable con las opciones de reserva
+    bookingOptions.forEach(option => {
+      const optionElement = document.createElement('option');
+      optionElement.value = option.name;
+      optionElement.textContent = option.name;
+      roomTypeSelect.appendChild(optionElement);
+    });
+
+    roomTypeSelect.addEventListener('change', (e) => {
+      const selectedIndex = e.target.selectedIndex;
+      const selectedOption = bookingOptions[selectedIndex];
+    });
   }
+});
+
+// Clase para representar una opción de reserva
+class BookingOption {
+  constructor(name, price, description, services = []) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.services = services;
+  }
+
   showInfo() {
     console.log(`
       name: ${this.name}
@@ -97,76 +172,28 @@ class BookingOption {
   }
 }
 
-let bookingOptionArray = [];
-
-document.addEventListener('DOMContentLoaded', function () {
-  const roomTypeSelect = document.getElementById('room-type');
-  
-  if (roomTypeSelect) {
-    roomTypeSelect.addEventListener('change', (e) => {
-
-      const selectedIndex = e.target.selectedIndex;
-      const selectedOption = e.target.options[selectedIndex];
-      const bookingOption = new BookingOption(selectedOption);
-
-      bookingOptionArray.push(bookingOption);
-      localStorage.setItem("bookingOptionArray", JSON.stringify(bookingOptionArray));
-      bookingOption.showInfo();
-    });
-  }
-});
-
-// Para Cargar opciones de reserva desde un archivo JSON
-fetch('../js/booking_options.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Error al obtener opciones. Código de estado HTTP: ${response.status}`);
-    }
-    return response.json();
-  })
+// Obtener los datos del archivo room_availability.json
+fetch('../js/room_availability.json')
+  .then(response => response.json())
   .then(data => {
-    bookingOptions = data.options;
-    localStorage.setItem('bookingOptions', JSON.stringify(bookingOptions));
-  })
-  .catch(error => {
-    console.error(error);
-  });
-
-// Disponibilidad de cuarto
-let rooms = [
-  { type: "cabañas", datesAvailable: ["2022-05-01", "2022-05-02", "2022-05-03"] },
-  { type: "habitaciones", datesAvailable: ["2022-05-01", "2022-05-02"] },
-  { type: "camping", datesAvailable: ["2022-05-03"] }
-];
-
-function checkAvailability(selectedType, checkIn, checkOut) {
-  let selectedRoom = rooms.find(room => room.type === selectedType);
-  if (!selectedRoom) {
-    return "Tipo de habitación no disponible";
-  }
-
-  let availableDates = selectedRoom.datesAvailable;
-  for (let date = new Date(checkIn); date < new Date(checkOut); date.setDate(date.getDate() + 1)) {
-    let formattedDate = date.toISOString().slice(0, 10);
-    if (!availableDates.includes(formattedDate)) {
-      return "Habitación no disponible en fechas seleccionadas";
+    function checkAvailability(selectedType, checkIn, checkOut) {
+      let selectedRoom = rooms.find(room => room.type === selectedType);
+      if (!selectedRoom) {
+        return "Tipo de habitación no disponible";
+      }
+    
+      let availableDates = selectedRoom.datesAvailable;
+      for (let date = new Date(checkIn); date < new Date(checkOut); date.setDate(date.getDate() + 1)) {
+        let formattedDate = date.toISOString().slice(0, 10);
+        if (!availableDates.includes(formattedDate)) {
+          return "Habitación no disponible en fechas seleccionadas";
+        }
+      }
+      return "Habitación disponible para reservar";
     }
-  }
-  return "Habitación disponible para reservar";
-}
+  })
+  .catch(error => console.error(error));
 
-let selectedType = "cabañas";
-let availability = checkAvailability(selectedType, checkIn, checkOut);
-console.log(availability);
-
-// Habitaciones de hotel y estado de ocupación
-let hotelRooms = [
-  {number: 1, type: 'cabañas', occupied: false},
-  {number: 2, type: 'habitaciones', occupied: true},
-  {number: 3, type: 'camping', occupied: false},
-  {number: 4, type: 'cabañas', occupied: true},
-  {number: 5, type: 'habitaciones', occupied: false}
-];
 
 // API absctapi calendario
 function httpGetAsync(url, callback) {
